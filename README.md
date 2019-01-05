3196ec88e3100ab7a537afec2c30b2b3ccf0e00d
walk8243 <walk8243@gmail.com>
2019-01-06 00:13:51 +0900

    GitHubのGuidesの「Example query」を実行できる環境の作成
    https://developer.github.com/v4/guides/forming-calls/#example-query

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # graphql-client-tutorial
 GitHubのAPI v4を使ったクライアントサイドのチュートリアル
+
+## Usage
+```.sh
+copy config/default.yaml config/development.yaml
+# config/development.yaml内の「xxxxxxxx」をGitHubから取得したトークンに書き換えてください。
+
+npm i && npm start
+```
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('index');
+const config = require('config');
+const client = require('graphql-client')({
+  url: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `Bearer ${config.github.token}`,
+  },
+});
+const fs = require('fs');
+
+client.query(fs.readFileSync('./query/test.query').toString(), {}, (req, res) => {
+  if(res.status === 401) {
+    throw new Error('Not authorized')
+  }
+})
+.then((body) => {
+  console.log(body);
+})
+.catch((error) => {
+  console.error(error);
+});
